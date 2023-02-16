f4f26791b0f5b74c392f37e9f834cf3f730424fc
georg00 <grzesiekb2013@gmail.com>
2023-02-16 13:21:51 +0100

    Added notes app to settings and added new model

--- NoteApp/notes/models.py
+++ NoteApp/notes/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    note_text = models.CharField(max_length=250)
+    add_date = models.DateTimeField('added date')
