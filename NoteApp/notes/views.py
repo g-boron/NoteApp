b5b7ba489a02008e665e3310a3387e87a07879ae
georg00 <grzesiekb2013@gmail.com>
2023-02-16 14:03:33 +0100

    Created basic views and templates

--- NoteApp/notes/views.py
+++ NoteApp/notes/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Note
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Test')
+    notes = Note.objects.all()
+    context = {'notes': notes}
+
+    return render(request, 'notes/index.html', context)
+
+
+def show(request, note_id):
+    note = get_object_or_404(Note, pk=note_id)
+    context = {'note': note}
+
+    return render(request, 'notes/show.html', context)
