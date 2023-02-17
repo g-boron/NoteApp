787371625160568917bfab1f1af2fb445072484b
georg00 <grzesiekb2013@gmail.com>
2023-02-17 13:56:48 +0100

    Edited sidebar and added css stylesheet

--- NoteApp/notes/urls.py
+++ NoteApp/notes/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:note_id>/', views.show, name='show'),
+    path('show_notes/', views.show_notes, name='show_notes'),
+    path('add_note/', views.add_note, name='add_note'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- NoteApp/notes/views.py
+++ NoteApp/notes/views.py
@@ -3,14 +3,22 @@ from .models import Note
 
 # Create your views here.
 def index(request):
+
+    return render(request, 'notes/index.html')
+
+
+def show_notes(request):
     notes = Note.objects.all()
     context = {'notes': notes}
 
-    return render(request, 'notes/index.html', context)
-
+    return render(request, 'notes/show_notes.html', context)
 
 def show(request, note_id):
     note = get_object_or_404(Note, pk=note_id)
     context = {'note': note}
 
     return render(request, 'notes/show.html', context)
+
+
+def add_note(request):
+    pass
