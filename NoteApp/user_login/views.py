eb3fcbe14d95adea4f6b55814e0ec291e0389198
georg00 <grzesiekb2013@gmail.com>
2023-02-19 20:56:51 +0100

    Added register page with crispy form

--- NoteApp/user_login/views.py
+++ NoteApp/user_login/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
 
 # Create your views here.
 def register(request):
-    return render()
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    else:
+        form = RegisterForm()
+
+    context = {'form': form}
+
+    return render(request, 'user_login/register.html', context)
