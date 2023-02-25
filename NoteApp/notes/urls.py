4385e14e7077a76ef36244b095308d02fbc212e4
georg00 <grzesiekb2013@gmail.com>
2023-02-25 14:05:13 +0100

    Changes indexview to generic and added displaying last user's note

--- NoteApp/notes/urls.py
+++ NoteApp/notes/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexPageView.as_view(), name='index'),
     path('<int:note_id>/', views.show, name='show'),
     path('show_notes/', views.show_notes, name='show_notes'),
     path('add_note/', views.add_note, name='add_note'),
--- NoteApp/notes/views.py
+++ NoteApp/notes/views.py
@@ -3,11 +3,20 @@ from .models import Note
 from .forms import AddNewNote
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
+from django.views.generic.base import TemplateView
 
 # Create your views here.
-def index(request):
+class IndexPageView(TemplateView):
+    template_name = 'notes/index.html'
 
-    return render(request, 'notes/index.html')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            context['last_note'] = Note.objects.filter(user=self.request.user).last()
+        else:
+            context['last_note'] = None
+
+        return context
 
 
 @login_required(login_url='/login/')
