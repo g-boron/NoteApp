de81807e8601841cb98a947f9f7e0203b354852c
Grzegorz Boroń <grzesiekb2013@gmail.com>
2023-03-15 19:51:42 +0100

    Changed labels in adding form

--- NoteApp/notes/forms.py
+++ NoteApp/notes/forms.py
@@ -6,3 +6,8 @@ class AddNewNote(forms.ModelForm):
     class Meta:
         model = Note
         fields = ['title', 'note_text', 'img', 'doc', ]
+
+    def __init__(self, *args, **kwargs):
+        super(AddNewNote, self).__init__(*args, **kwargs)
+        self.fields['img'].label = "Image"
+        self.fields['doc'].label = "File"
