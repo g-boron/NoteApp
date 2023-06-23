871de846fc0cd31bc695d03927cf91bb0c76cc28
Grzegorz Boroń <grzesiekb2013@gmail.com>
2023-06-23 10:28:05 +0200

    Changed forms size

--- notes/forms.py
+++ notes/forms.py
@@ -4,11 +4,17 @@ from django.forms.widgets import ClearableFileInput
 
 
 class AddNewNote(forms.ModelForm):
-    file_field = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}), required=False)
+    file_field = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True, 'style': 'font-size: 22px;'}), required=False)
 
     class Meta:
         model = Note
         fields = ['title', 'note_text', 'category', ]
+        widgets = {
+            'category': forms.Select(attrs={'style': 'font-size: 22px;'}),
+            'title': forms.TextInput(attrs={'style': 'font-size: 22px;'}),
+            'note_text': forms.Textarea(attrs={'style': 'font-size: 22px;'}),
+        }
+        
 
     def __init__(self, *args, **kwargs):
         super(AddNewNote, self).__init__(*args, **kwargs)
@@ -27,3 +33,8 @@ class EditProfileForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email', ]
+        widgets = {
+            'first_name': forms.TextInput(attrs={'style': 'font-size: 22px;'}),
+            'last_name': forms.TextInput(attrs={'style': 'font-size: 22px;'}),
+            'email': forms.EmailInput(attrs={'style': 'font-size: 22px;'}),
+        }
